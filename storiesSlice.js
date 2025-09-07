7bace33e73458fc19e76645170dd936848331f39
ZIADMOHAMED-A8 <badry8977@gmail.com>
2025-09-07 16:34:11 +0300

    update the backend link

--- storiesSlice.js
+++ storiesSlice.js
@@ -3,7 +3,7 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 const apiSlice=createApi({
     reducerPath:'api',
     baseQuery:fetchBaseQuery({
-        baseUrl:'stories-dummybackend-production.up.railway.app'
+        baseUrl:'https://stories-dummybackend-production.up.railway.app'
     }),
     endpoints:builder=>({
         getStories:builder.query({
